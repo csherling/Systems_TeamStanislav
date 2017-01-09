d65081d2c4df36d581b4e9c6f58538a2d4b10ff2
esandine <esandine@stuy.edu>
2017-01-09 11:01:19 -0500

    Started physics

---
+++ physics.c
@@ -0,0 +1,16 @@
+#define PLAYER_HEIGHT 5
+//struct arrow has initial velocity and angle, and current coordinates
+typedef struct arrow{float velocity; float theta; float x; float y;}arrow;
+typedef struct player{char health; float xcor;}player;
+
+char shoot(/*arrow *a,*/ player *p, float v, float theta){
+  float arrow_x=p->xcor;
+  float arrow_y = 0;
+  char unpassed = 1;//Checks if arrow passed the player
+  while(a->y>=0){
+    if((v>0)&&(arrow_x>p->xcor)&&(unpassed))
+  }
+}
+int main(){
+  return 0;
+}
