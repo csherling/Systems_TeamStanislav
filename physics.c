830d3c83d5eec9df0af5d50a15b73a38d02bd759
esandine <esandine@stuy.edu>
2017-01-12 11:01:04 -0500

    Have not tested physics

--- physics.c
+++ physics.c
@@ -3,13 +3,57 @@
 typedef struct arrow{float velocity; float theta; float x; float y;}arrow;
 typedef struct player{char health; float xcor;}player;
 
-char shoot(/*arrow *a,*/ player *p, float v, float theta){
-  float arrow_x=p->xcor;
-  float arrow_y = 0;
-  char unpassed = 1;//Checks if arrow passed the player
-  while(a->y>=0){
-    if((v>0)&&(arrow_x>p->xcor)&&(unpassed))
+/*Params:
+ p: player who is shooting the arrow
+ v: initial velocity positive and negative
+ Returns:
+ 1-Hit
+ 0-Miss
+ */
+char shoot(player *shooter, player *target, float v, float theta){
+  float arrow_x=shooter->xcor;
+  float arrow_y = PLAYER_HEIGHT;
+  while(arrow_y>=0){
+    if(v>0){
+      if(arrow_x>=target->x){
+	if(arrow_y<=PLAYER_HEIGHT){
+	  kill(*target);
+	  return 1;
+	}else{
+	  overshoot(arrow_y);
+	  return 0;
+	}
+      }
+    }else{
+      if(arrow_x<=target->x){
+	if(arrow_y<=PLAYER_HEIGHT){
+	  kill(*target);
+	  return 1;
+	}else{
+	  return 0;
+	}
+      }
+    }
   }
+  return 0;
+}
+
+void processCoor(float x, float y){
+  printf("(%f,%f)\n",x,y);
+}
+
+void kill(player *p){
+  p->health-=1;
+  printf("HIT! new health: %d\n",p->health);
+}
+
+
+void overshoot(int height){
+  printf("MISSED! overshot by %d\n",height);
+}
+
+void undershoot(int distance){
+  printf("MISSED! undershot by %d\n",distance);
 }
 int main(){
   return 0;
