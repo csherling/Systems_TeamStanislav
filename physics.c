8c2103d67227ff252e2fd5c1bec898b38d4803c2
esandine <esandine@stuy.edu>
2017-01-12 14:47:36 -0500

    It all compiles, now I gotta test

--- physics.c
+++ physics.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "physics.h"
 
 #define PLAYER_HEIGHT 5
+#define GRAVITY 9.8
 //struct arrow has initial velocity and angle, and current coordinates
 
 /*Params:
@@ -15,7 +17,10 @@
 char shoot(player *shooter, player *target, float v, float theta){
   float arrow_x=shooter->xcor;
   float arrow_y = PLAYER_HEIGHT;
+  float vx = v*cos(theta);
+  float vy = v*sin(theta);
   while(arrow_y>=0){
+    //Checks for collisions and if it has hit player
     if(v>0){
       if(arrow_x>=target->xcor){
 	if(arrow_y<=PLAYER_HEIGHT){
@@ -36,6 +41,10 @@ char shoot(player *shooter, player *target, float v, float theta){
 	}
       }
     }
+    //Movements
+    arrow_x+=vx;
+    arrow_y+=vy;
+    vy-=GRAVITY;
   }
   undershoot(target->xcor-arrow_x);
   return 0;
@@ -59,5 +68,11 @@ void undershoot(int distance){
   printf("MISSED! undershot by %d\n",distance);
 }
 int main(){
+  player p1;
+  p1.xcor = 0;
+  p1.health = 1;
+  player p2;
+  p2.xcor = 100;
+  p2.health = 1;
   return 0;
 }
