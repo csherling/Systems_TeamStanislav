08946c5ba59e3847836c449c36a98b47730f4321
esandine <esandine@stuy.edu>
2017-01-13 11:01:40 -0500

    ADd physics

--- physics.c
+++ physics.c
@@ -18,8 +18,9 @@
 char shoot(player *shooter, player *target, float v, float theta){
   float arrow_x=shooter->xcor;
   float arrow_y = PLAYER_HEIGHT;
-  float vx = v*cos(theta);
-  float vy = v*sin(theta);
+  processCor(arrow_x,arrow_y);
+  float vx = v*cos(theta)/100;
+  float vy = v*sin(theta)/100;
   while(arrow_y>=0){
     //Checks for collisions and if it has hit player
     if(v>0){
@@ -27,9 +28,6 @@ char shoot(player *shooter, player *target, float v, float theta){
 	if(arrow_y<=PLAYER_HEIGHT){
 	  kill(target);
 	  return 1;
-	}else{
-	  overshoot(arrow_y);
-	  return 0;
 	}
       }
     }else{
@@ -37,21 +35,24 @@ char shoot(player *shooter, player *target, float v, float theta){
 	if(arrow_y<=PLAYER_HEIGHT){
 	  kill(target);
 	  return 1;
-	}else{
-	  return 0;
 	}
       }
     }
     //Movements
     arrow_x+=vx;
     arrow_y+=vy;
-    vy-=GRAVITY;
+    vy-=GRAVITY/10000;
+    processCor(arrow_x, arrow_y);
   }
-  undershoot(target->xcor-arrow_x);
+  if(arrow_x>target->xcor){
+    overshoot(arrow_x-target->xcor);
+  }else{
+    undershoot(target->xcor-arrow_x);
+  }
   return 0;
 }
 
-void processCoor(float x, float y){
+void processCor(float x, float y){
   printf("(%f,%f)\n",x,y);
 }
 
@@ -86,7 +87,7 @@ int main(){
     theta=input;
     strcpy(velocity,strsep(&theta, ","));
     printf("Velocity: %s\nTheta: %s\n",velocity, theta);
-    going=shoot(&p1, &p2, atof(velocity), atof(theta));
+    going=shoot(&p1, &p2, atof(velocity), atof(theta)*M_PI/180);
   }
   return 0;
 }
