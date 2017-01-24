c27349049622b22cdf0bf1b929bdec167db8d88f
Wilson Berkow <wilsonberkow@gmail.com>
2017-01-23 23:20:45 -0500

    Add constants.h (should have been in prev commit)

---
+++ constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+
+// TOTAL width of game, across all displays
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+// Number of displays to play game on:
+#define DISPLAYS 2
+#define DISPLAY_WIDTH (SCREEN_WIDTH / DISPLAYS)
+
+#endif
